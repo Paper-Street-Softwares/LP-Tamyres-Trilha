d509c4d4abbc3fb2a037724fc7bcb71f2dd87e30
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-13 09:52:05 -0300

    fix: Remove padding e adiciona mainFont na minitag da Hero

--- src/components/sections/Steps.jsx
+++ src/components/sections/Steps.jsx
@@ -26,7 +26,7 @@ export default function Steps({ colorMode }) {
   const titleColor = textClasses[colorMode] || textClasses.default;
 
   return (
-    <SectionArea className={`${bgClass}`} paddingbot={true}>
+    <SectionArea className={`${bgClass}`} paddingbot={false}>
       <SectionWrapper className="flex flex-col desktop1:flex-row-reverse gap-[40px] desktop2:gap-0 desktop1:justify-between">
         <MotionDivDownToUp className="relative w-[90%] desktop1:w-[415px] desktop2:w-[450px] flex justify-center rounded-xl shadow-custom-opacity shadow-shadowSteps/10">
           {/* <GalleryAbout /> */}
